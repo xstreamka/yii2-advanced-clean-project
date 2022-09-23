befcd599fb27e280497fa15896566be6a32385aa
p.durtsev <p.durtsev@agatgroup.com>
2022-09-23 15:16:32 +0300

    dev

--- console/config/main.php
+++ console/config/main.php
@@ -29,6 +29,20 @@ return [
                     'class' => \yii\log\FileTarget::class,
                     'levels' => ['error', 'warning'],
                 ],
+                // Фиксирование остальных ошибок на почту.
+                [
+                    'class' => 'yii\log\EmailTarget',
+                    'mailer' => 'mailer',
+                    'levels' => ['error'],
+                    'except' => [
+                        'yii\web\HttpException:404',
+                    ],
+                    'message' => [
+                        'from' => $params['errorEmail'],
+                        'to' => $params['supportEmail'],
+                        'subject' => implode(' ', $_SERVER['argv'] ?? []) . " Ошибка 50* на сайте {$params['host']} (console)",
+                    ],
+                ],
             ],
         ],
     ],
